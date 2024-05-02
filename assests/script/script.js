5c615f782ac2bf3723832b60b476fe02b2085cb8
lahiruileperumage <lahiru212001@gmail.com>
2024-05-02 17:24:46 +0530

    update nav bar active button

--- assests/script/script.js
+++ assests/script/script.js
@@ -20,3 +20,23 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('fa-x');
     navbar.classList.toggle('active');
 };
+
+/*========================================*/
+let section = document.querySelectorAll('section');
+let navLikns = document.querySelectorAll('header nav a');
+
+window.onscroll= () =>{
+    section.forEach(sec =>{
+        let top = window.screenY;
+        let offset = sec.offsetTop - 150;
+        let height =sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top>= offset && top < offset + height){
+            navLikns.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href='+id+']').classList.add('active');
+            })
+        }
+    })
+}
