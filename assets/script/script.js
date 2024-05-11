78bc95616e1e11cadb435115246375da80237290
lahiruileperumage <lahiru212001@gmail.com>
2024-05-11 12:42:35 +0530

    update

--- assets/script/script.js
+++ assets/script/script.js
@@ -70,7 +70,7 @@ const typed =new Typed('.animation-text',{
 const cursor = document.querySelector('.cursor');
 
 document.addEventListener('mousemove', e => {
-    cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;")
+    cursor.setAttribute("style", "top: " + (e.pageY - 5) + "px; left: " + (e.pageX - 5) + "px;")
 });
 
 document.addEventListener('click', e => {
